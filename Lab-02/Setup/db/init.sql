30921cab1cb8083b490c98c5973e0a7a70d7c14f
SanyuktaNair <143982936+SanyuktaNair@users.noreply.github.com>
2023-10-18 13:35:42 -0600

    Update init.sql

--- Lab-02/Setup/db/init.sql
+++ Lab-02/Setup/db/init.sql
@@ -1,24 +1,24 @@
+CREATE TABLE breed_type (
+    breed_id INT PRIMARY KEY,
+    breed VARCHAR(255)
+);
+
 CREATE TABLE animal_data (
     animal_id VARCHAR(255) PRIMARY KEY,
-    breed_id VARCHAR(255),
+    breed_id INT,
     name VARCHAR(255),
     date_of_birth DATE,
     animal_type VARCHAR(255),
     FOREIGN KEY (breed_id) REFERENCES breed_type(breed_id)
 );
 
-CREATE TABLE breed_type (
-    breed_id INT PRIMARY KEY,
-    breed VARCHAR(255)
-);
-
 CREATE TABLE outcome_events (
     outcome_event_id INT PRIMARY KEY,
     datetime TIMESTAMP,
     animal_id VARCHAR(255),
-    outcome_type_id INT,
-    FOREIGN KEY (animal_id) REFERENCES animal_data(animal_id),
-    FOREIGN KEY (outcome_type_id) REFERENCES outcome_type(outcome_type_id)
+    outcome_type VARCHAR(255),
+    FOREIGN KEY (animal_id) REFERENCES animal_data(animal_id)
+    
 );
 
 CREATE TABLE fact_table (
@@ -28,5 +28,4 @@ CREATE TABLE fact_table (
     FOREIGN KEY (animal_id) REFERENCES animal_data(animal_id),
     FOREIGN KEY(breed_id) REFERENCES breed_type(breed_id),
     FOREIGN KEY (outcome_event_id) REFERENCES outcome_events(outcome_event_id)
-    
 );
